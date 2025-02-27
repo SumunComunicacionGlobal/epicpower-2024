efa128cb65d172a4c27e104196106aa1006653bb
Fernando Blasco <fernando@sumun.net>
2025-02-27 16:17:32 +0100

    Product page update

--- loop-templates/content-single.php
+++ loop-templates/content-single.php
@@ -62,16 +62,20 @@ global $post;
 
 		<?php if ( $product_excerpt ) : ?>
 
-		<div class="row my-5">
-			<div class="col-md-6 col-lg-7">
-				<?php if ( isset( $product_excerpt ) ) {
-					echo '<h2 class="h4">' . sprintf( __( '%s overview', 'epicpower' ), get_the_title() ) . '</h2>';
-					echo $product_excerpt;
-				} ?>
+		<div class="product-overview-wrapper border p-4 my-5 rounded-3">
+
+			<div class="row">
+				<div class="col-md-6 col-lg-7">
+					<?php if ( isset( $product_excerpt ) ) {
+						echo '<h2 class="h4">' . sprintf( __( '%s overview', 'epicpower' ), get_the_title() ) . '</h2>';
+						echo $product_excerpt;
+					} ?>
+				</div>
+				<div class="col-md-6 col-lg-5">
+					<?php echo get_the_post_thumbnail( $post->ID, 'large', array('class' => 'img-fluid') ); ?>
+				</div>
 			</div>
-			<div class="col-md-6 col-lg-5">
-				<?php echo get_the_post_thumbnail( $post->ID, 'large', array('class' => 'img-fluid') ); ?>
-			</div>
+
 		</div>
 		
 		<?php endif; ?>
