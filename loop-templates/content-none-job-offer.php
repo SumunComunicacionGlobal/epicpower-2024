6786904f1d36cc38d4430fbfcf705ae28fb9de21
Fernando Blasco <fernando@sumun.net>
2025-03-24 12:36:55 +0100

    String translation

--- loop-templates/content-none-job-offer.php
+++ loop-templates/content-none-job-offer.php
@@ -10,5 +10,5 @@ defined( 'ABSPATH' ) || exit;
 ?>
 
 <div class="p-4 bg-light mb-4 no-job-offers">
-	<p class="mb-0"><?php esc_html_e( 'There are no active job offers at the moment. You can fill out the spontaneous application form below if you wish.', 'understrap' ); ?></p>
+	<p class="mb-0"><?php esc_html_e( 'There are no active job offers at the moment. You can fill out the spontaneous application form below if you wish.', 'epicpower' ); ?></p>
 </div>
